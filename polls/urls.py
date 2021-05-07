48ae7d04819015b8effb847226bdc6fa6b369b69
Justin Lavoie <justin.lavoie@mail.com>
2021-05-06 20:12:11 -0400

    added view and template for book pages

--- polls/urls.py
+++ polls/urls.py
@@ -6,7 +6,17 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:book_id>/', views.book, name='book'),
+    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+
+# app_name = 'polls'
+# urlpatterns = [
+#     path('', views.IndexView.as_view(), name='index'),
+#     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+#     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
--- polls/views.py
+++ polls/views.py
@@ -4,21 +4,80 @@ from django.urls import reverse
 from django.utils import timezone
 from django.views import generic
 
-from .models import Choice, Question
+from .models import Choice, Question, Books
 
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+    context_object_name = 'tolkien_book_list'
 
     def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
+        return Books.objects.filter(
+            authors="J. R. R. Tolkien"
+        ).order_by('year')[:5]
+
+# class IndexView(generic.ListView):
+#     template_name = 'polls/index.html'
+#     context_object_name = 'latest_question_list'
+
+#     def get_queryset(self):
+#         """
+#         Return the last five published questions (not including those set to be
+#         published in the future).
+#         """
+#         return Question.objects.filter(
+#             pub_date__lte=timezone.now()
+#         ).order_by('-pub_date')[:5]
+
+
+def book(request, book_id):
+    book = get_object_or_404(Books, id=book_id)
+
+    if not book.cover_image:
+        book.cover_image = "/static/polls/images/default_book_image.svg"
+    elif "isfdb.org" in book.cover_image:
+        try:
+            image_loc = book.cover_image.split("/images/")[1]
+            book.cover_image = "/static/polls/images/isfdb/" + image_loc
+        except:
+            book.cover_image = "/static/polls/images/default_book_image.svg"
+
+    if not book.pages:
+        book.pages = "Unknown number of"
+
+    book.book_type = book.book_type[0] + book.book_type[1:].lower()
+
+    template_data = dict(
+        title_id=book_id,
+        title=book.title,
+        year=book.year,
+        authors=book.authors,
+        book_type=book.book_type,
+        isbn=book.isbn,
+        pages=book.pages,
+        editions=book.editions,
+        alt_titles=book.alt_titles,
+        series_str_1 = book.series_str_1,
+        series_str_2 = book.series_str_2,
+        original_lang=book.original_lang,
+        original_title=book.original_title,
+        original_year=book.original_year,
+        isfdb_rating=book.isfdb_rating,
+        award_winner=book.award_winner,
+        virtual = book.virtual,
+        juvenile=book.juvenile,
+        stand_alone=book.stand_alone,
+        inconsistent=book.inconsistent,
+        cover_image=book.cover_image,
+        wikipedia=book.wikipedia,
+        synopsis=book.synopsis,
+        note=book.note
+
+    )
+
+    rendered_page = render(request, "polls/book.html", template_data)
+    if rendered_page is not None:
+        return rendered_page
 
 
 class DetailView(generic.DetailView):
