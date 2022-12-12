8ba701243f8ee0c9625415985605b677f0419409
Justin Lavoie <justin.lavoie@mail.com>
2022-12-11 19:34:27 -0500

    Change url() to re_path for upgrade to Django 4

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-from django.conf.urls import url
+from django.urls import include, path, re_path
 
 from . import views
 from django.contrib.auth import views as auth_views
@@ -24,41 +23,41 @@ urlpatterns = [
 	path('', include('recsys.urls'), name='home'),
     path('recsys/', include('recsys.urls')),
     
-    url(r'^(accounts/)?signup/.*$', 
+    re_path(r'^(accounts/)?signup/.*$',
         views.SignUpView.as_view(), name='signup'),
-    url(r'^(accounts/)?login/.*$', 
+    re_path(r'^(accounts/)?login/.*$',
         auth_views.LoginView.as_view(), name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 
-    url(r'^account/$', views.Account.as_view(), name='account'),
+    re_path(r'^account/$', views.Account.as_view(), name='account'),
     
-    url(r'^password_reset/$', 
-        auth_views.PasswordResetView.as_view(), 
+    re_path(r'^password_reset/$',
+        auth_views.PasswordResetView.as_view(),
         name='password_reset'),
-    url(r'^password_reset/done/$', 
-        auth_views.PasswordResetDoneView.as_view(), 
+    re_path(r'^password_reset/done/$',
+        auth_views.PasswordResetDoneView.as_view(),
         name='password_reset_done'),
     path('reset/<uidb64>/<token>/',
-        auth_views.PasswordResetConfirmView.as_view(), 
+        auth_views.PasswordResetConfirmView.as_view(),
         name='password_reset_confirm'),
-    url(r'^reset/done/$', 
-        auth_views.PasswordResetCompleteView.as_view(), 
+    re_path(r'^reset/done/$',
+        auth_views.PasswordResetCompleteView.as_view(),
         name='password_reset_complete'),
 
-    url(r'^password_change/$', auth_views.PasswordChangeView.as_view(
+    re_path(r'^password_change/$', auth_views.PasswordChangeView.as_view(
             template_name='registration/password_change.html'),
         name='password_change'),
-    url(r'^password_change_done/$',
+    re_path(r'^password_change_done/$',
         auth_views.PasswordChangeDoneView.as_view(),
         name='password_change_done'),
 
-    url(r'^username_change/$', 
+    re_path(r'^username_change/$',
         views.UserNameChangeView.as_view(), name='username_change'),
-    url(r'^displayname_change/$', views.DisplayNameChangeView.as_view(),
+    re_path(r'^displayname_change/$', views.DisplayNameChangeView.as_view(),
         name='displayname_change'),
-    url(r'^email_change/$', views.EmailChangeView.as_view(),
+    re_path(r'^email_change/$', views.EmailChangeView.as_view(),
         name='email_change'),
-    url(r'^account_delete/$', 
+    re_path(r'^account_delete/$',
         views.AccountDeleteView.as_view(), name='account_delete'),
     path('admin/', admin.site.urls),
 ]
